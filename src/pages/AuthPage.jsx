3636f43b04862b93a8dec14538b74ef54a07d71e
ilker şendoğan <128185341+ilkhukuk@users.noreply.github.com>
2023-11-11 23:22:40 +0300

    Update AuthPage.jsx

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,13 +1,15 @@
-import { signInWithPopup } from "firebase/auth";
-import { auth, provider } from "./../firebase/config";
+import React from 'react';
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from './../firebase/config';
 
 const AuthPage = ({ setIsAuth }) => {
   // giriş yapma fonk.
   const handleClick = () => {
     signInWithPopup(auth, provider)
       .then((res) => {
+        console.log(res);
         // oturumunun açık olduğunu bildiren token'ı lokal'de saklama
-        localStorage.setItem("token", res.user.refreshToken);
+        localStorage.setItem('token', res.user.refreshToken);
         // yetkiyi true'ya çek
         setIsAuth(true);
       })
@@ -17,12 +19,14 @@ const AuthPage = ({ setIsAuth }) => {
   return (
     <div className="container">
       <div className="auth">
-        <h1>Chat Odası</h1>
-        <p>Devam etmek için Griş Yapın</p>
+        <h1>Yıldız Chat Odası</h1>
+        <p>Devam etmek için Giriş Yapın</p>
+        <div className="ilk">
         <button onClick={handleClick}>
           <img src="/g-logo.png" />
-          <span>Google ile gir</span>
+          <span>Lütfen tıklayın</span>
         </button>
+        </div>
       </div>
     </div>
   );
